49b704fb5b8c1ef52c62ad821ec8585a69b8376f
reda97dz <rbaka79@gmail.com>
2021-08-15 18:59:13 +0100

    1.7: unicafe step2

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -12,7 +12,11 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
+  const all = good + bad + neutral
+  const average = (good + bad * -1)/all
+  const positive = (good/all)*100
+  
   const handleGoodClick = () => {
     setGood(good + 1)
   }
@@ -35,6 +39,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   )
 }
