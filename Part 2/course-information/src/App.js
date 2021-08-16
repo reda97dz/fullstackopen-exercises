6a71726ff301035507f293d47a959bff2c808b4f
reda97dz <rbaka79@gmail.com>
2021-08-16 16:08:30 +0100

    2.1: Course information step6

---
+++ Part 2/course-information/src/App.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+
+const Header = ({title}) => {
+  return (
+    <h1>{title}</h1>
+  )
+}
+
+const Part = ({part}) => {
+  return (
+    <p>{part.name} {part.exercises}</p>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map(part => 
+        <Part key={part.id} part={part} /> 
+      )}
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header title={course.name}/>
+      <Content parts={course.parts}/>
+    </div>
+    
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
