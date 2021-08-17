fe4f67c4c1d9eb413f2ba0f857db7fc35f4c3147
reda97dz <rbaka79@gmail.com>
2021-08-17 10:59:21 +0100

    2.9*: The Phonebook Step4

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '041-32-02-05' }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
-
+  const [personsToShow, setPersonsToShow] = useState(persons)
+  const [newFilter, setFilter] = useState('')
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
@@ -35,15 +40,27 @@ const App = () => {
       window.alert(`A person with the number ${nameObject.number} already exists`)
     }else{
       setPersons(persons.concat(nameObject))
+      setPersonsToShow(persons)
     }
 
     setNewName('')
     setNewNumber('')
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+    const regex = new RegExp(newFilter, 'i')
+    const filteredPersons = () => persons.filter(person => person.name.match(regex))
+    setPersonsToShow(filteredPersons)
+}
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with <input value={newFilter} onChange={handleFilterChange} />
+      </div>
+      <h2>Add New</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -56,7 +73,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
+      {personsToShow.map(person => 
         <p key={person.number}>{person.name} {person.number}</p>)}
     </div>
   )
