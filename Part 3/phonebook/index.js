170658079a46b214aef0326ddd927148409ac605
reda97dz <rbaka79@gmail.com>
2021-08-22 10:34:43 +0100

    3.9 phonebook backend step9

--- Part 3/phonebook/index.js
+++ Part 3/phonebook/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+
 const morgan = require('morgan')
+
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
 
@@ -100,7 +105,7 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
