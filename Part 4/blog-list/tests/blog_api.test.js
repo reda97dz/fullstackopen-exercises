7df176d157d9f80a611ba2bcbff1cfcc91a1d57a
reda97dz <rbaka79@gmail.com>
2021-09-23 00:09:26 +0100

    4.8: Blog list tests, step1

--- Part 4/blog-list/tests/blog_api.test.js
+++ Part 4/blog-list/tests/blog_api.test.js
@@ -3,37 +3,18 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
-
-const initialBlogs = [
-    {
-        _id: "5a422ba71b54a676234d17fb",
-        title: "TDD harms architecture",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-        likes: 0,
-        __v: 0
-    },
-    {
-        _id: "5a422bc61b54a676234d17fc",
-        title: "Type wars",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-        likes: 2,
-        __v: 0
-        
-    }
-]
+const helper = require('./list_helper')
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlogs[0])
-    await blogObject.save()
-    blogObject = new Blog(initialBlogs[1])
-    await blogObject.save()
+    const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
 })
 
 test('all blogs are returned', async () => {
     const response = await api.get('/api/blogs').expect(200).expect('Content-type', /application\/json/)
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
 }, 100000)
 
 afterAll(() => {
