ef4836122f9cb1359ee1ad6bfc44ea5efacf07b3
reda97dz <rbaka79@gmail.com>
2021-09-22 23:57:53 +0100

    first api test

---
+++ Part 4/blog-list/tests/blog_api.test.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        _id: "5a422ba71b54a676234d17fb",
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0,
+        __v: 0
+    },
+    {
+        _id: "5a422bc61b54a676234d17fc",
+        title: "Type wars",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        likes: 2,
+        __v: 0
+        
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+})
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs').expect(200).expect('Content-type', /application\/json/)
+}, 100000)
+
+afterAll(() => {
+    mongoose.connection.close()
+})
