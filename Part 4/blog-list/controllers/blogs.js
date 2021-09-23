8a1aee9c825e106c42d4afad0e76c05df41f664a
reda97dz <rbaka79@gmail.com>
2021-09-23 17:28:11 +0100

    4.15: bloglist expansion, step3

--- Part 4/blog-list/controllers/blogs.js
+++ Part 4/blog-list/controllers/blogs.js
@@ -1,15 +1,38 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
-    response.json(blogs)
+    const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
+    response.json(blogs.map(blog => blog.toJSON()))
 })
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
   
-blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response, next) => {
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if(!token || !decodedToken.id){
+        return response.status(401).json({error: 'token missing or invalid'})
+    }
+    const user = await User.findById(decodedToken.id)
+
+    const blog = new Blog({
+        ...request.body,
+        user: user._id
+    })
+
     const savedBlog = await blog.save()
-    response.json(savedBlog)
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.json(savedBlog.toJSON())
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
--- Part 4/blog-list/models/blog.js
+++ Part 4/blog-list/models/blog.js
@@ -1,11 +1,27 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const blogSchema = new mongoose.Schema({
-    title: {type: String, required: true},
+    title: {
+      type: String,
+      required: true
+    },
     author: String,
-    url: {type: String, required: true},
-    likes: {type: Number, default: 0}
+    url: {
+      type: String,
+      required: true
+    },
+    likes: {
+      type: Number,
+      default: 0
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
 })
+
+blogSchema.plugin(uniqueValidator)
 
 blogSchema.set('toJSON', {
     transform: (document, returnedObject) => {
