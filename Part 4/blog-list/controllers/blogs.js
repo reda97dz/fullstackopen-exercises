a8f00e5463f9cd217ff1a5ef64414a00b2ef6355
reda97dz <rbaka79@gmail.com>
2021-09-24 13:44:31 +0100

    4.22*: bloglist expansion, step10

--- Part 4/blog-list/controllers/blogs.js
+++ Part 4/blog-list/controllers/blogs.js
@@ -8,15 +8,9 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
     const token = request.token
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-
-    if(!token || !decodedToken.id){
-        return response.status(401).json({error: 'token missing or invalid'})
-    }
-
-    const user = await User.findById(decodedToken.id)
+    const user = request.user
 
     const blog = new Blog({
         ...request.body,
@@ -31,12 +25,8 @@ blogsRouter.post('/', async (request, response, next) => {
 
 blogsRouter.delete('/:id', async (request, response) => {
     const token = request.token
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-
-    if(!token || !decodedToken.id){
-        return response.status(401).json({error: 'token missing or invalid'})
-    }
-    const user = await User.findById(decodedToken.id)
+    const user = request.user
+    
     const blog = await Blog.findById(request.params.id)
     
     if(blog.user._id.toString() === user._id.toString()){
