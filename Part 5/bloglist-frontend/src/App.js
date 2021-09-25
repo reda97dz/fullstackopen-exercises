338cd2195dc74008e99b3654600742e081de04ed
reda97dz <rbaka79@gmail.com>
2021-09-25 22:35:45 +0100

    5.5 Blog list frontend, step5

--- Part 5/bloglist-frontend/src/App.js
+++ Part 5/bloglist-frontend/src/App.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
+import Togglable from './components/Togglable'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -10,10 +12,7 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [message, setMessage] = useState(null)
-
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
+  const blogRef = React.createRef()
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -47,27 +46,16 @@ const App = () => {
     }
   }
 
-  const addBlog = (event) => {
-    event.preventDefault()
-    const blogObject = {
-      title: title,
-      author: author,
-      url: url
+  const addBlog = async (blogObject) => {
+    try{
+      blogRef.current.toggleVisibility()
+      const createdBlog = await blogService.create(blogObject)
+      setBlogs(blogs.concat(createdBlog))
+    } catch (expection){
+      console.log("error")
     }
-    blogService
-      .create(blogObject)
-        .then(returnedBlog => {
-          setBlogs(blogs.concat(returnedBlog))
-          setTitle('')
-          setAuthor('')
-          setUrl('')
-          setMessage(`Blog "${title}" by ${author} was added`)
-          setTimeout(() => {
-            setMessage(null)
-          }, 5000);
-        })
+    
   }
-  
 
   if (user === null) {
 
@@ -99,23 +87,13 @@ const App = () => {
           {user.name} logged-in
           <button onClick={()=>{window.localStorage.removeItem('loggedBlogAppuser'); setUser(null)}}>logout</button>
         </p>
+        
+        <Togglable buttonLabel="add blog" ref={blogRef}>
+          <BlogForm createBlog={addBlog}/>
+        </Togglable>
       </div>
-      <h2>create new</h2>
-      <form onSubmit={addBlog}>
-        <div>
-          title:
-          <input type="text" name="title" value={title} onChange={({target})=>setTitle(target.value)}/>
-        </div>
-        <div>
-          author:
-          <input type="text" name="author" value={author} onChange={({target})=>setAuthor(target.value)}/>
-        </div>
-        <div>
-          url:
-          <input type="text" name="url" value={url} onChange={({target})=>setUrl(target.value)}/>
-        </div>
-        <button type="submit">create</button>
-      </form>
+      
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
